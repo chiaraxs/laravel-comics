8c748ea00637ea4e09b9d5f52a2b9173094f8270
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-03-03 17:16:55 +0100

    Add: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,70 @@
-<footer> questo è il mio footer </footer>
+<footer>
+    <div class="footer-left">
+
+        <ul>
+            <li>
+                <h3>dc comics</h3>
+            </li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+        </ul>
+
+
+        <ul>
+            <li>
+                <h3>shop</h3>
+            </li>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+        </ul>
+
+
+
+
+        <ul>
+            <li>
+                <h3>dc</h3>
+            </li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+
+
+
+
+        <ul>
+            <li>
+                <h3>sites</h3>
+            </li>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+        </ul>
+
+    </div>
+
+    <div class="footer-right-logo">
+        <img src="{{ asset('img/dc-logo-bg.png')}}" alt="">
+    </div>
+
+</footer>
+
 
 
 
